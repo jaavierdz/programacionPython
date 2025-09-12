ed0ce807901065fe091ea4920557edbf0ea0e8a4
Javier Rodríguez-Alarcón <javier@javierdz.me>
2025-09-12 13:51:34 +0200

    Improve program exit message formatting

--- prueba_1.py
+++ prueba_1.py
@@ -9,7 +9,7 @@ from colorama import Fore
 
 # Handler para cuando le das Ctrl+C 
 def sig_handler(signal, frame):
-    print(Fore.RED + "[!]" + Fore.RESET + " Saliendo...")
+    print(Fore.RED + "\n[!]" + Fore.RESET + " Saliendo...")
     exit(0)
 
 signal.signal(signal.SIGINT, sig_handler)
